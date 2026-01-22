42cb9b531a620398f7473bf4a9106ec224ebd53f
karthik kolukuluru <karthikkolukuluru28@gmail.com>
2026-01-22 12:30:29 +0530

    add

--- src/main/java/com/lifestyle/dto/AuthResponse.java
+++ src/main/java/com/lifestyle/dto/AuthResponse.java
@@ -1,3 +1,23 @@
+//package com.lifestyle.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//
+//@Data
+//@AllArgsConstructor
+//public class AuthResponse {
+//
+//    private String message;
+//    private String token;
+//
+//    public AuthResponse(String message, String token) {
+//        this.message = message;
+//        this.token = token;
+//    }
+//
+//    public String getMessage() { return message; }
+//    public String getToken() { return token; }
+//}
 package com.lifestyle.dto;
 
 import lombok.AllArgsConstructor;
@@ -9,12 +29,4 @@ public class AuthResponse {
 
     private String message;
     private String token;
-
-    public AuthResponse(String message, String token) {
-        this.message = message;
-        this.token = token;
-    }
-
-    public String getMessage() { return message; }
-    public String getToken() { return token; }
 }
